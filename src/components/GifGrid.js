b0e537ea5efa764af19779a114c90c2a7d81fe5f
Jose Ricardo Mendoza Castañeda <mendozacastanedajosericardo@gmail.com>
2022-01-03 20:51:29 -0600

    App Finalizada y funcionando

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,21 +1,25 @@
-import React, { Fragment, useEffect, useState } from 'react'
-import { getGifs } from '../helpers/getGifs'
+import React, { Fragment } from 'react'
+import useFetchGifs from '../hooks/useFetchGifs'
 import GifGridItem from './GifGridItem'
 
 const GifGrid = ({ category }) => {
-    const [images, setImages] = useState([])
-
-    useEffect(() => {
-        getGifs( category )
-            .then( imgs => setImages( imgs ))
-    }, [ category ])
+    // const [images, setImages] = useState([])
+    // useEffect(() => {
+    //     getGifs( category )
+    //         .then( imgs => setImages( imgs ))
+    // }, [ category ])
 
     // useEffect( funcion(), dependencias ) .- Va a ejecutar el codigo, solo si se cambia de estado
     // una dependencia, en este caso lo ponemos para que solo se ejecute una vez
 
+    // Pusimos useEffect, en el hook, useFetchGifs
+
+    const { data: images, loading } = useFetchGifs( category )
+
     return (
         <Fragment>
-            <h3> {category} </h3>
+            <h3 className='animate__animated animate__fadeIn'> {category} </h3>
+            { loading && <p className='animate__animated animate__flash'> Cargando... </p> }
             <div className='card-grid'>
 
                 {
